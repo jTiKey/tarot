7fd2ae7592491f69a74c30f72c275a7d6818a724
slava <slava.khromyak@gmail.com>
2018-10-22 13:31:55 +0300

    add timer

--- tarot/static/js/project.js
+++ tarot/static/js/project.js
@@ -1,1 +1,24 @@
-/* Project specific Javascript goes here. */
+function ShowTimes() {
+  var now = new Date();
+  var hrs = checkTime(23-now.getUTCHours());
+  var mins = checkTime(59-now.getUTCMinutes());
+  var secs = checkTime(59-now.getUTCSeconds());
+  var str = hrs+':'+mins+':'+secs;
+  refresh(hrs, mins, secs);
+  document.getElementById('countdownToMidnight').innerHTML = str;
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  }
+  return i;
+}
+
+function refresh(hrs, mins, secs) {
+	if (hrs == 0 && mins == 0 && secs == 0) {
+    	location.reload();
+    }
+}
+
+setInterval('ShowTimes()',1000);
