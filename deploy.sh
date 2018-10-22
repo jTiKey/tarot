d8a10f3f402edeba1387ac73e50ca3e4ecd322f8
slava <slava.khromyak@gmail.com>
2018-10-22 12:08:49 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+echo 'Stopping server...'
 supervisorctl stop all
 source venv/bin/activate
+echo 'Pulling...'
 git pull
-
+echo 'Migrating...'
 python manage.py migrate --settings=config.settings.production
-python manage.py collectstatic --settings=config.settings.production
+echo 'Collecting...'
+python manage.py collectstatic --no-input --settings=config.settings.production
+echo 'Translating...'
 python manage.py compilemessages -l uk --settings=config.settings.production
 python manage.py compilemessages -l en --settings=config.settings.production
-
+echo 'Starting...'
 supervisorctl start all
