a2bba4d59dced4bb0969a522b06a8d8c96627d7b
slava <slava.khromyak@gmail.com>
2018-10-22 13:32:19 +0300

    add timer

--- tarot/static/js/project.js
+++ tarot/static/js/project.js
@@ -1,23 +1,23 @@
 function ShowTimes() {
-  var now = new Date();
-  var hrs = checkTime(23-now.getUTCHours());
-  var mins = checkTime(59-now.getUTCMinutes());
-  var secs = checkTime(59-now.getUTCSeconds());
-  var str = hrs+':'+mins+':'+secs;
-  refresh(hrs, mins, secs);
-  document.getElementById('countdownToMidnight').innerHTML = str;
+    var now = new Date();
+    var hrs = checkTime(23-now.getUTCHours());
+    var mins = checkTime(59-now.getUTCMinutes());
+    var secs = checkTime(59-now.getUTCSeconds());
+    var str = hrs+':'+mins+':'+secs;
+    refresh(hrs, mins, secs);
+    document.getElementById('countdownToMidnight').innerHTML = str;
 }
 
 function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i
-  }
-  return i;
+    if (i < 10) {
+        i = "0" + i
+    }
+    return i;
 }
 
 function refresh(hrs, mins, secs) {
-	if (hrs == 0 && mins == 0 && secs == 0) {
-    	location.reload();
+    if (hrs == 0 && mins == 0 && secs == 0) {
+        location.reload();
     }
 }
 
