ad71fd96f9c0f1b708fb6aeec6557e92ea81ff9a
slava <slava.khromyak@gmail.com>
2018-10-25 14:53:19 +0300

    add filter

--- tarot/readings/admin.py
+++ tarot/readings/admin.py
@@ -9,6 +9,7 @@ class ReadingAdmin(admin.ModelAdmin):
     fields = ('question', 'name', 'response', 'image', 'email', 'ip_address', 'responded', 'created', )
     readonly_fields = ('created', 'ip_address', 'name', 'question', 'email')
     list_display = ('email', 'created', 'responded', )
+    list_filter = ('responded', )
 
 
 admin.site.register(models.Reading, ReadingAdmin)
